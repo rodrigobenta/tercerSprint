40ef42e2f9dc60e7e282768090020c6c5b92470f
Mateo Tambasco <mateo.tambasco12@gmail.com>
2022-10-06 15:52:40 -0300

    tests finalizados

--- tests/token.test.js
+++ tests/token.test.js
@@ -1,25 +1,22 @@
-
 const {app} = require("../app");
 const request = require("supertest");
-const db = require("../database/models");
-const babel = require("../babel.config");
-const {generateJWT} = require('../helpers/generateJWT');
-const bcrypt = require("bcrypt");
-const { sequelize } = require('../database/models');
-const jwt = require('jsonwebtoken');
+const jwt = require('../helpers/generateJWT');
 
-
-
-describe('probando', () => {
-    // test("Listar usuarios con token roto" , async() => {
-    //     const token3 = await jwt(null);
-
-    //     console.log(token3);
-    //     expect(token3).rejects(expect.any(String));
+describe('Tests de token', () => {
+    test('Token invalido', async () => {
+        const {statusCode, body} = await request(app).get('/api/v2/users').auth(null, { type: 'bearer' }); 
+        expect(statusCode).toEqual(401);
+        expect(body).toEqual(expect.objectContaining({
+            ok: expect.any(Boolean),
+            msg: expect.any(String)
+        }));
+    });
+   /*  test('rejects to No se pudo crear el token', async () => {
+        try{
+            await jwt();
+        }catch(error){
+            expect(error).toEqual(expect.any(String))
+        }
         
-    // });
-
-    test('rejects to No se pudo crear el token', async () => {
-        await expect(jwt.reject(new Error('No se pudo crear el token'))).rejects.toThrow('No se pudo crear el token');
-      });
+    }); */
 })
